1c14c5e3964ba1d6a155f4ece389502f2fb391f5
Orest Ivasiv <halyph@gmail.com>
2019-03-24 23:53:45 +0100

    Step 3: Added Intro

--- src/main/scala/fpmax/App.scala
+++ src/main/scala/fpmax/App.scala
@@ -31,3 +31,27 @@ object App {
     }
   }
 }
+
+object Intro {
+  /**
+    * Functional programming is all about programming with functions.
+    * Functions are:
+    *
+    * 1. Total. For every input, they return an output.
+    * 2. Deterministic. For the same input, they return the same output.
+    * 3. Pure. Their only effec is computing the return value.
+    *
+    * These properties help us:
+    *
+    * 1. Reason about our programs using equational reason.
+    * 2. Refactor our program without changing their meaning.
+    * 3. Test our programs more easily
+    * 4. Invert control so caller always has control over callee
+    * 5. Reason about our program using type-based reasoning
+    *
+    */
+
+  def println(s: String): Unit = ???
+  def readLine(): String = ???
+  def parseInt(s: String): Int = ???
+}
---
+++ src/main/scala/fpmax/fpmap.scala
@@ -0,0 +1,5 @@
+package fpmax
+
+class fpmap {
+
+}
