aa7ab159d9aaaa1d565eca3a586fe53577ec9e96
Orest Ivasiv <halyph@gmail.com>
2019-03-25 01:12:14 +0100

    Step 5: Added IO[A] and refactored the app

--- src/main/scala/fpmax/App.scala
+++ src/main/scala/fpmax/App.scala
@@ -7,40 +7,54 @@ object App {
 
   def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
 
+  final case class IO[A](unsafeRun: () => A) { self =>
+    def map[B](f: A => B): IO[B] = IO(() => f(self.unsafeRun()))
+
+    def flatMap[B](f: A => IO[B]): IO[B] = IO(() => f(self.unsafeRun()).unsafeRun())
+  }
+  object IO {
+    def point[A](a: => A): IO[A] = IO(() => a)
+  }
+
+  def putStrLn(line: String): IO[Unit] = IO(() => println(line))
+  def getStrLn: IO[String] = IO(() => readLine())
+
+  def nextInt(upper: Int): IO[Int] = IO(() => scala.util.Random.nextInt(upper))
+
+  def checkContinue(name: String): IO[Boolean] =
+    for {
+      _     <- putStrLn("Do you want to continue, " + name + "?")
+      input <- getStrLn.map(_.toLowerCase)
+      cont  <- input match {
+                  case "y" => IO.point(true)
+                  case "n" => IO.point(false)
+                  case _ => checkContinue(name)
+                }
+    } yield cont
+
+  def gameLoop(name: String): IO[Unit] =
+    for {
+      num   <- nextInt(5).map(_ + 1)
+      n     <- putStrLn("Dear " + name + ", please guess a number from 1 to 5:")
+      input <- getStrLn
+      _     <- parseInt(input).fold(
+                 putStrLn("You did not enter the number")
+               )(guess =>
+                if (guess == num) putStrLn("You guessed right, " + name + "!")
+                else putStrLn("You guessed wrong, " + name + "! The number was: " + num)
+               )
+      cont  <- checkContinue(name)
+      _     <- if(cont) gameLoop(name) else IO.point(())
+    } yield ()
+
   def main(args: Array[String]): Unit = {
-    println("What is your name?")
+    for {
+      _    <- putStrLn("What is your name?")
+      name <- getStrLn
+      _    <- putStrLn("Hello, " + name + ", welcome to the game!")
+      _    <- gameLoop(name)
+    } yield ()
 
-    val name = readLine()
-
-    println("Hello, " + name + ", welcome to the game!")
-
-    var exec = true
-
-    while (exec) {
-      val num = scala.util.Random.nextInt(5) + 1
-
-      println("Dear " + name + ", please guess a number from 1 to 5:")
-
-      val guess = parseInt(readLine())
-      guess match {
-        case None => println("You did not enter the number")
-        case Some(guess) =>
-          if (guess == num) println("You guessed right, " + name + "!")
-          else println("You guessed wrong, " + name + "! The number was: " + num)
-      }
-
-      var cont = true
-      while (cont) {
-        cont = false
-        println("Do you want to continue, " + name + "?")
-
-        readLine().toLowerCase match {
-          case "y" => exec = true
-          case "n" => exec = false
-          case _ => cont = false
-        }
-      }
-    }
   }
 }
 
