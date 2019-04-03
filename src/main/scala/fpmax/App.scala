1a5fb58e2ca740cd4ed83365d165f24f040d87a0
Orest Ivasiv <halyph@gmail.com>
2019-04-03 03:44:57 +0200

    Step 7: Added TestIO

--- src/main/scala/fpmax/App.scala
+++ src/main/scala/fpmax/App.scala
@@ -24,7 +24,7 @@ object App {
     def flatMap[B](afb: A => F[B])(implicit F: Program[F]): F[B] = F.chain(fa, afb)
   }
 
-  def point[F[_], A](a: => A)(implicit F: Program[F]): F[A] = F.finish(a)
+//  def point[F[_], A](a: => A)(implicit F: Program[F]): F[A] = F.finish(a)
 
   trait Console[F[_]] {
     def putStrLn(line: String): F[Unit]
@@ -33,10 +33,6 @@ object App {
   object Console {
     def apply[F[_]](implicit F: Console[F]): Console[F] = F
 
-    implicit val ConsoleIO = new Console[IO] {
-      def putStrLn(line: String): IO[Unit] = IO(() => println(line))
-      def getStrLn: IO[String] = IO(() => readLine())
-    }
   }
 
   def putStrLn[F[_]: Console](line: String): F[Unit] = Console[F].putStrLn(line)
@@ -48,8 +44,48 @@ object App {
   object Random {
     def apply[F[_]](implicit F: Random[F]): Random[F] = F
 
-    implicit val RandomIO = new Random[IO] {
-      def nextInt(upper: Int): IO[Int] = IO(() => scala.util.Random.nextInt(upper))
+  }
+
+  case class TestData(input: List[String], output: List[String], nums: List[Int]) {
+    def putStrLn(line: String): (TestData, Unit) =
+      (copy(output = line :: output), ())
+
+    def getStrLn: (TestData, String) =
+      (copy(input = input.drop(1)), input.head)
+
+    def nextInt(upper: Int): (TestData, Int) =
+      (copy(nums = nums.drop(1)), nums.head)
+
+    def showResults = output.reverse.mkString("\n")
+  }
+
+  case class TestIO[A](run: TestData => (TestData, A)) { self =>
+    def map[B](ab: A => B): TestIO[B] =
+      TestIO(t => self.run(t) match { case (t, a) => (t, ab(a)) })
+
+    def flatMap[B](afb: A => TestIO[B]): TestIO[B] =
+      TestIO(t => self.run(t) match { case (t, a) => afb(a).run(t) })
+
+    def eval(t: TestData): TestData = run(t)._1
+
+  }
+
+  object TestIO {
+    def point[A](a: => A): TestIO[A] = TestIO(t => (t, a))
+
+    implicit val ProgramTestIO = new Program[TestIO] {
+      def finish[A](a: => A): TestIO[A] = TestIO.point(a)
+
+      def chain[A, B](fa: TestIO[A], afb: A => TestIO[B]): TestIO[B] = fa.flatMap(afb)
+
+      def map[A, B](fa: TestIO[A], ab: A => B): TestIO[B] = fa.map(ab)
+    }
+    implicit val ConsoleTestIO = new Console[TestIO] {
+      def putStrLn(line: String): TestIO[Unit] = TestIO(t => t.putStrLn(line))
+      def getStrLn: TestIO[String] = TestIO(t => t.getStrLn)
+    }
+    implicit val RandomTestIO = new Random[TestIO] {
+      def nextInt(upper: Int): TestIO[Int] = TestIO(t => t.nextInt(upper))
     }
   }
 
@@ -65,13 +101,17 @@ object App {
 
     implicit val ProgramIO = new Program[IO] {
       def finish[A](a: => A): IO[A] = IO.point(a)
+
       def chain[A, B](fa: IO[A], afb: A => IO[B]): IO[B] = fa.flatMap(afb)
+
       def map[A, B](fa: IO[A], ab: A => B): IO[B] = fa.map(ab)
     }
-
-    implicit  val ConsoleIO = new Console[IO] {
+    implicit val ConsoleIO = new Console[IO] {
       def putStrLn(line: String): IO[Unit] = IO(() => println(line))
       def getStrLn: IO[String] = IO(() => readLine())
+    }
+    implicit val RandomIO = new Random[IO] {
+      def nextInt(upper: Int): IO[Int] = IO(() => scala.util.Random.nextInt(upper))
     }
   }
 
@@ -88,29 +128,47 @@ object App {
                 }
     } yield cont
 
+  def printResults[F[_]: Console](input: String, num: Int, name: String): F[Unit] =
+    parseInt(input).fold(
+      putStrLn("You did not enter a number")
+    )(guess =>
+      if (guess == num) putStrLn("You guessed right, " + name + "!")
+      else putStrLn("You guessed wrong, " + name + "! The number was: " + num)
+    )
+
   def gameLoop[F[_]: Program: Console: Random](name: String): F[Unit] =
     for {
       num   <- nextInt(5).map(_ + 1)
       n     <- putStrLn("Dear " + name + ", please guess a number from 1 to 5:")
       input <- getStrLn
-      _     <- parseInt(input).fold(
-                 putStrLn("You did not enter the number")
-               )(guess =>
-                if (guess == num) putStrLn("You guessed right, " + name + "!")
-                else putStrLn("You guessed wrong, " + name + "! The number was: " + num)
-               )
+      _     <- printResults(input, num, name)
       cont  <- checkContinue(name)
       _     <- if(cont) gameLoop(name) else finish(())
     } yield ()
 
-  def main(args: Array[String]): Unit = {
+  def main[F[_]: Program: Random: Console]: F[Unit] =
     for {
-      _    <- putStrLn("What is your name?")
-      name <- getStrLn
-      _    <- putStrLn("Hello, " + name + ", welcome to the game!")
-      _    <- gameLoop(name)
+      _     <- putStrLn("What is your name?")
+      name  <- getStrLn
+      _     <- putStrLn("Hello, " + name + ", welcome to the game!")
+      _     <- gameLoop(name)
     } yield ()
 
+  def mainIO: IO[Unit] = main[IO]
+
+  def mainTestIO: TestIO[Unit] = main[TestIO]
+
+  val TestExample =
+    TestData(
+      input  = "John" :: "1" :: "n" :: Nil,
+      output = Nil,
+      nums   = 0 :: Nil
+    )
+
+  def runTest = mainTestIO.eval(TestExample).showResults
+
+  def main(args: Array[String]): Unit = {
+    println(runTest)
   }
 }
 
